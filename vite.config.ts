e5e44bfa01c9d0267bc73bd4ef9adc8ae18a2d1d
UsmanAhri <Roland03052018>
2023-05-19 09:22:28 +0300

    config fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      refTransform: 'true',
-    }),
-  ],
+  plugins: [vue()],
+  base: '',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
